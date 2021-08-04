f7b56c5410013188b0aff3b05f6cc0e3f33b64fa
wams <andrew@lightbulbux.com>
2021-08-05 00:27:12 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: 'michael',
+        name: 'Michael Smith',
+        handle: 'mikey',
+        avatarURL: 'http://localhost:5001/michael.jpg',
+      },
+      {
+        id: 'ryan',
+        name: 'Ryan Joe',
+        handle: 'ryanjoe',
+        avatarURL: 'http://localhost:5001/ryan.jpg',
+      },
+      {
+        id: 'tyler',
+        name: 'Tyler McGinnis',
+        handle: 'tylermcginnis',
+        avatarURL: 'http://localhost:5001/tyler.jpg',
+      },
+    ],
+  }
+
+  removeContact = (contact) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((c) => {
+        return c.id !== contact.id
+      }),
+    }))
+  }
   render() {
     return (
-      <div>
-        Hello World
-      </div>
+      <ListContacts
+        contacts={this.state.contacts}
+        onDeleteContact={this.removeContact}
+      />
     )
   }
 }
 
-export default App;
+export default App
